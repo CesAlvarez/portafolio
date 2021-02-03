a5c8321c7a6cd9d22379c2080aef0ea67315435a
César Alvarez <cesalvarez25@gmail.com>
2021-02-03 10:10:00 -0300

    Creando carrusel de imagen

--- header.php
+++ header.php
@@ -1,21 +1,25 @@
 <!-- header -->
 <header class="header-main">
 
-    <!-- logotipo -->
-    <div class="logotipo">
+    <div class="header-box">
+
+        <!-- logotipo -->
+        <div class="logotipo">
+            <h2>LOGO</h2>
+        </div>
+        <!-- /logotipo -->
+
+        <!-- menu -->
+        <nav class="navegacion">
+
+            <a href="">Inicio</a>
+            <a href="">Portafolio</a>
+            <a href="">Contactos</a>
+
+        </nav>
+        <!-- /menu -->
 
     </div>
-    <!-- /logotipo -->
-
-    <!-- menu -->
-    <nav class="navegacion">
-
-        <a href="">Inicio</a>
-        <a href="">Portafolio</a>
-        <a href="">Contactos</a>
-
-    </nav>
-    <!-- /menu -->
 
 </header>
 <!-- /header -->
--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Portafolio</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/glider-js@1.7.3/glider.min.css">
     <link rel="stylesheet" href="css/main.css">
 </head>
 <body>
@@ -23,6 +24,10 @@
         ?>
 
     </div>
+    
+    <script src="https://cdn.jsdelivr.net/npm/glider-js@1.7.3/glider.min.js"></script>
+	<script src="https://kit.fontawesome.com/2c36e9b7b1.js" crossorigin="anonymous"></script>
+    <script src="js/script.js"></script>
     
 </body>
 </html>
